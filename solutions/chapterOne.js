1012a9723620d77a8c915b3f77c293e127b292d5
Ryan Atkinson <rjatkinson2@gmail.com>
2015-05-21 11:44:03 -0700

    Add solution for chapter one problem number three

--- solutions/chapterOne.js
+++ solutions/chapterOne.js
@@ -1,14 +1,16 @@
 
-// Important to know whether ASCII string or a Unicode String
-// Need to consider all characters  
-// Constant space complexity with ASCII because you max out at 128 possible characters.
 
 var module = module || undefined;
 if(module){
   module.exports = {};
   module.exports.uniqueString = uniqueString;
   module.exports.reverseString = reverseString;
+  module.exports.anagram = anagram;
 }
+
+// Important to know whether ASCII string or a Unicode String
+// Need to consider all characters  
+// Constant space complexity with ASCII because you max out at 128 possible characters.
 
 function uniqueString(string){
   var uniques = {};
@@ -23,6 +25,8 @@ function uniqueString(string){
   return true;
 }
 
+// Consider duplicates, 
+
 function reverseString(string){
   var store;
   var ars = string.split('');
@@ -36,4 +40,23 @@ function reverseString(string){
     }
   }
   return ars.join('');
+}
+
+function anagram(s1, s2){
+  var charCount = {};
+  if(s1.length !== s2.length) return false;
+  for(var i = 0; i < s1.length; i++){
+    if(charCount.hasOwnProperty(s1.charAt(i))){
+      charCount[s1.charAt(i)]++;
+    }else{
+      charCount[s1.charAt(i)] = 1;
+    }
+  }for(var j = 0; j < s2.length; j++){
+    if(charCount.hasOwnProperty(s2.charAt(j)) && charCount[s2.charAt(j)] > 0){
+      charCount[s2.charAt(j)]--;
+    }else{
+      return false;
+    }
+  }
+  return true;
 }
--- test/chapterOneTest.js
+++ test/chapterOneTest.js
@@ -7,6 +7,7 @@ var assert = assert || require('chai').assert;
 
 var uniqueString = uniqueString || methods.uniqueString;
 var reverseString = reverseString || methods.reverseString;
+var anagram = anagram || methods.anagram;
 
 describe('Chapter One of Cracking the Coding Interview', function(){
   describe('1.1: String has all unique characters', function(){
@@ -31,6 +32,21 @@ describe('Chapter One of Cracking the Coding Interview', function(){
     });
     it('should reverse a string of duplicate characters', function(){
       expect(reverseString('!!!!!!!!!!!!')).to.equal('!!!!!!!!!!!!');
+    });
+  });
+  
+  describe('1.3: Is an anagram', function(){
+    it('should return false if strings are not the same length', function(){
+      expect(anagram('bananas', 'bananats')).to.equal(false);
+    });
+    it('should return true for same length but different characters', function(){
+      expect(anagram('bananas', 'bananats')).to.equal(false);
+    });
+    it('should return true for random characters', function(){
+      expect(anagram('#!@$%AB12@!#$$$', '$$$#!@21BA%$@!#')).to.equal(true);
+    });
+    it('should return true for repeat letters', function(){
+      expect(anagram('ffffffff', 'ffffffff')).to.equal(true);
     });
   });
 });
