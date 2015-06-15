ffd82ea8ae377e9baabd4bc6fb0dc9781ddfc9a1
Ryan Atkinson <rjatkinson2@gmail.com>
2015-06-15 14:34:10 -0700

    Add solution and tests for 2.2

--- solutions/chapterTwo.js
+++ solutions/chapterTwo.js
@@ -1,26 +1,43 @@
 var module = module || undefined;
-if(module){
+if (module) {
   module.exports = {};
   module.exports.removeDuplicates = removeDuplicates;
+  module.exports.findK2Last = findK2Last;
 }
 
 // No buffer allowed!
 // Linked Lists can often be approached using a walker and a runner which reference one another to complete tasks.
-function removeDuplicates(head){
+function removeDuplicates(head) {
   var walker = head;
   var runner = head;
-  while(walker.next !== null){
-    while(runner.next !== null){
-      if(runner.next.val === walker.val){
+  while (walker.next !== null) {
+    while (runner.next !== null) {
+      if (runner.next.val === walker.val) {
         runner.next = runner.next.next;
-      }
-      else{
+      } else {
         runner = runner.next;
       }
     }
-    if(walker.next === null){ return head; }
+    if (walker.next === null) { return head; }
     walker = walker.next;
     runner = walker;
   }
   return head;
+}
+
+// Tabulated values in diagramming phaes should match the order of operations in while loop.
+// Explicitly define base case, before while loop conditions in tabulated values.
+// Remember that kth to last means the last value isn't included in your count to k.
+// Also, no need to change the stored k-value until AFTER your greater than k+1.
+
+function findK2Last(head, k) {
+  var current = head, total = 1, kth = head;
+  while (current.next !== null) {
+    current = current.next;
+    total++;
+    if (total > k + 1) {
+      kth = kth.next;
+    }
+  }
+  return total >= k + 1 ? kth : undefined;
 }
--- test/chapterTwoTest.js
+++ test/chapterTwoTest.js
@@ -9,6 +9,7 @@ var expect = expect || require('chai').expect;
 var assert = assert || require('chai').assert;
 
 var removeDuplicates = removeDuplicates || methods.removeDuplicates;
+var findK2Last = findK2Last || methods.findK2Last;
 
 // Linked List Implementation.
 var LinkedList = function(){
@@ -42,33 +43,54 @@ LinkedList.prototype.removeHead = function(){
 // Write tests below:
 
 describe('Chapter Two of Cracking the Coding Interview', function(){
-  describe('2.1: Remove duplicates from LinkedList', function(){
-    
-    var linkedList, head;
-    
-    beforeEach(function(){
-      console.log('testing beforeEach');
-      linkedList = new LinkedList();
-      head = new linkedList.createNode(5);
-      linkedList.addToTail(head);
-    });
+  
+  var linkedList, head;
+  
+  beforeEach(function(){
+    console.log('testing beforeEach');
+    linkedList = new LinkedList();
+    head = new linkedList.createNode(5);
+    linkedList.addToTail(head);
+  });
 
+  describe('2.1: Remove duplicates from LinkedList', function(){
     it('should remove duplicates from a linked list', function(){
-      linkedList.addToTail(new linkedList.createNode(9));
-      linkedList.addToTail(new linkedList.createNode(5));
-      linkedList.addToTail(new linkedList.createNode(9));
-      linkedList.addToTail(new linkedList.createNode(4));
+      var nodes = [9,5,9,4];
+      nodes.forEach(function(val){
+        linkedList.addToTail(new linkedList.createNode(val));
+      });
 
       expect(removeDuplicates(head).next.val).to.equal(9);
       expect(removeDuplicates(head).next.next.val).to.equal(4);
     });
     it('handle a list of all duplicate values and return head node with next pointer to null', function(){
-      linkedList.addToTail(new linkedList.createNode(5));
-      linkedList.addToTail(new linkedList.createNode(5));
-      linkedList.addToTail(new linkedList.createNode(5));
-      linkedList.addToTail(new linkedList.createNode(5));
+      var nodes = [5,5,5,5,5,5,5,5,5];
+      nodes.forEach(function(val){
+        linkedList.addToTail(new linkedList.createNode(val));
+      });
       
       expect(removeDuplicates(head).next).to.equal(null);
+    });
+  });
+
+  describe('2.2: Find kth to last node from a linked list', function(){
+    it('should return the kth to last node from a linked list', function(){
+      var nodes = [9,5,9,4];
+      nodes.forEach(function(val){
+        linkedList.addToTail(new linkedList.createNode(val));
+      });
+
+      expect(findK2Last(head, 2).val).to.equal(5);
+      expect(findK2Last(head, 3).val).to.equal(9);
+      expect(findK2Last(head, 4).val).to.equal(5);
+    });
+    it('should return undefined if k is greater than or equal to the linked list length', function(){
+      var nodes = [9,5,9,4];
+      nodes.forEach(function(val){
+        linkedList.addToTail(new linkedList.createNode(val));
+      });
+
+      expect(findK2Last(head, 5)).to.equal(undefined);
     });
   });
 });
